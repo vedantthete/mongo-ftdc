4a20fba06303d3a5977ef7a1423c8d8ef0b80444
vedantthete <vedantthete@gmail.com>
2023-10-28 18:04:10 +0530

    Update metrics.go

--- metrics.go
+++ metrics.go
@@ -104,7 +104,7 @@ func (m *Metrics) ProcessFiles(filenames []string) error {
 		port = 3030
 		if len(filenames) > 3 { // avoid OOM killer
 			fmt.Println("* limits to latest 3 files in a Docker container")
-			filenames = filenames[len(filenames)-3:]
+			filenames = filenames[len(filenames)-30:]
 		}
 	}
 	diag := NewDiagnosticData()
